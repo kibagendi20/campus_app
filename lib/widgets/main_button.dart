b7dd37935a12ac2d5ed26bb2fc8877fff3a27d50
Wesley Kibagendi <wesoon98@gmail.com>
2022-07-19 09:23:02 +0300

    Added implementation of App's button

---
+++ lib/widgets/main_button.dart
@@ -0,0 +1,27 @@
+import 'package:campus_app/utils/colors/colors.dart';
+import 'package:flutter/widgets.dart';
+
+class MainButton extends StatelessWidget {
+  const MainButton({Key? key, required this.text, required this.buttonColor}) : super(key: key);
+  final String text;
+  final Color buttonColor;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.08,
+      decoration: BoxDecoration(
+        color: buttonColor,
+        borderRadius: BorderRadius.circular(15)
+      ),
+      child: Text(text,style: const TextStyle(
+        color: AppColors.textColor,
+        fontSize: 16
+      )
+      )
+    );
+  }
+}
