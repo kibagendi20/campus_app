b11f84c05f24f1bc2c705ef90596f9fcf3ba785f
Wesley Kibagendi <wesoon98@gmail.com>
2022-07-19 09:24:33 +0300

    Updated home screen

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:campus_app/utils/colors/colors.dart';
+import 'package:campus_app/widgets/main_button.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          image: DecorationImage(
+            fit: BoxFit.cover,
+                  image: AssetImage("images/main_homepage.jpg")
+          )
+        ),
+        child: Column(
+          children: [
+            Expanded(child: Container()),
+            Container(
+              margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+              child:  InkWell(onTap:(){},child:const MainButton(text: "Get Started", buttonColor: AppColors.buttonColor)),
+            ),
+          ],
+        ),
+      ) ,
+    );
+  }
+}
